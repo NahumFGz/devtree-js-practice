19af79f064075b924cd746fc9f1ccba34001ad49
NahumFGz <ngutierrez2507@gmail.com>
2025-03-12 18:29:53 -0500

    feat: ejemplos de types

--- a_me_devtree/backend/src/index.ts
+++ a_me_devtree/backend/src/index.ts
@@ -6,3 +6,27 @@ const port = process.env.PORT || 4000
 server.listen(port, () => {
   console.log(colors.blue.bold(`Servidor funcionando en el puerto: ${port}`))
 })
+
+let productName = 'Tablet'
+let isAuth = false
+let price = 30
+
+type Product = {
+  id: number
+  price: number
+  name: string
+}
+
+let product: Product = {
+  id: 1,
+  price: 30,
+  name: 'tablet',
+}
+
+let product2: Product = {
+  id: 2,
+  price: 30,
+  name: 'tablet 11 pulgadas',
+}
+
+const numbers = [10, 20, 30]
