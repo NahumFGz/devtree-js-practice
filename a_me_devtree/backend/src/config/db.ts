95d07fff147bec4c6848e8ece3ffd161df26046d
NahumFGz <ngutierrez2507@gmail.com>
2025-03-11 21:18:13 -0500

    feat: :sparkles: cambio en .env

--- a_me_devtree/backend/src/config/db.ts
+++ a_me_devtree/backend/src/config/db.ts
@@ -5,11 +5,10 @@ dotenv.config()
 
 export const connectDB = async () => {
   try {
-    const url = `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASS}@cluster0.d52ot.mongodb.net/linktree_node_typescript`
-    const { connection } = await mongoose.connect(url)
+    const { connection } = await mongoose.connect(process.env.MONGO_URL)
 
-    const url2 = `${connection.host}:${connection.port}`
-    console.log(`MongoDB Conectado en ${url2}`)
+    const url = `${connection.host}:${connection.port}`
+    console.log(`MongoDB Conectado en ${url}`)
   } catch (error) {
     console.log(error.message)
     process.exit(1)
