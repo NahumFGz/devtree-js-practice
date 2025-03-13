3d5a61e199bc9526735bf3cf367e4b5801ca3df4
NahumFGz <ngutierrez2507@gmail.com>
2025-03-13 12:46:09 -0500

    feat: :ambulance: Cambio de versión de types

--- a_me_devtree/backend/src/handlers/index.ts
+++ a_me_devtree/backend/src/handlers/index.ts
@@ -2,11 +2,17 @@ import type { Request, Response } from 'express'
 import User from '../models/User'
 
 export const createAccount = async (req: Request, res: Response) => {
+  const { email } = req.body
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    const error = new Error('El usuario ya está registrado')
+    return res.status(409).json({ error: error.message })
+  }
   //!1. Forma 1
   //await User.create(req.body)
   //!2. Forma 2
   const user = new User(req.body)
   await user.save()
 
-  res.send({ msg: 'Registro creado correctamente' })
+  res.status(201).send({ msg: 'Registro creado correctamente' })
 }
