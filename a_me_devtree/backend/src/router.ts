6d1a04703f689ee59eebec83c771398e94af51fb
NahumFGz <ngutierrez2507@gmail.com>
2025-03-13 14:57:02 -0500

    feat: :sparkles: Se agregan las validaciones

--- a_me_devtree/backend/src/router.ts
+++ a_me_devtree/backend/src/router.ts
@@ -1,9 +1,19 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
 import { createAccount } from './handlers'
 
 const router = Router()
 
 /** Autentificación y Registro **/
-router.post('/auth/register', createAccount)
+router.post(
+  '/auth/register',
+  body('handle').notEmpty().withMessage('El handle no puede ir vacío'),
+  body('name').notEmpty().withMessage('El nombre no puede ir vacío'),
+  body('email').isEmail().withMessage('E-mail no válido'),
+  body('password')
+    .isLength({ min: 8 })
+    .withMessage('El password es muy corto, mínimo 8 caracteres'),
+  createAccount
+)
 
 export default router
