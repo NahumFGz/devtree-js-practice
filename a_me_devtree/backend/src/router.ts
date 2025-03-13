180abd4dcbacd1477a29b0def5907a83732cab52
NahumFGz <ngutierrez2507@gmail.com>
2025-03-13 12:17:51 -0500

    feat: :sparkles: Se agregan los types de los modelos

--- a_me_devtree/backend/src/router.ts
+++ a_me_devtree/backend/src/router.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express'
+import User from './models/User'
 
 const router = Router()
 
 /** Autentificación y Registro **/
-router.post('/auth/register', (req, res) => {
-  console.log('Desde Register')
-  console.log(req.body)
+router.post('/auth/register', async (req, res) => {
+  //!1. Forma 1
+  //await User.create(req.body)
+  //!2. Forma 2
+  const user = new User(req.body)
+  await user.save()
+
+  res.send({ msg: 'Registro creado correctamente' })
 })
 
 export default router
