64f39add42b5567ad22f477cb56515b385749075
NahumFGz <ngutierrez2507@gmail.com>
2025-03-13 13:00:38 -0500

    feat: :sparkles: Hashear contraseñas

--- a_me_devtree/backend/src/handlers/index.ts
+++ a_me_devtree/backend/src/handlers/index.ts
@@ -1,17 +1,18 @@
 import type { Request, Response } from 'express'
 import User from '../models/User'
+import { hashPassword } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-  const { email } = req.body
+  const { email, password } = req.body
+
   const userExists = await User.findOne({ email })
   if (userExists) {
     const error = new Error('El usuario ya está registrado')
     return res.status(409).json({ error: error.message })
   }
-  //!1. Forma 1
-  //await User.create(req.body)
-  //!2. Forma 2
+
   const user = new User(req.body)
+  user.password = await hashPassword(password)
   await user.save()
 
   res.status(201).send({ msg: 'Registro creado correctamente' })
