353cac05851bdd3d7ec22b59776f0c18519bc04b
NahumFGz <ngutierrez2507@gmail.com>
2025-03-14 00:46:07 -0500

    feat: :sparkles: Layouts y Outlets

--- a_me_devtree/frontend/src/views/RegisterView.tsx
+++ a_me_devtree/frontend/src/views/RegisterView.tsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom'
 export default function RegisterView() {
   return (
     <>
-      <div className='text-6xl'>Register View</div>
-
       <nav>
         <Link to='/auth/login'>¿ Ya estás registrado?, inicia sesion aquí</Link>
       </nav>
