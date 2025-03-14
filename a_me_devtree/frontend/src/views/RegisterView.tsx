d7066f5e4e32a14d16baefedc4f15b1c4ff9bc54
NahumFGz <ngutierrez2507@gmail.com>
2025-03-14 15:50:34 -0500

    feat: :sparkles: Se agrega config a axios

--- a_me_devtree/frontend/src/views/RegisterView.tsx
+++ a_me_devtree/frontend/src/views/RegisterView.tsx
@@ -2,8 +2,9 @@ import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import type { RegisterForm } from '../types'
 import { ErrorMessage } from '../components/ErrorMessage'
-import axios, { isAxiosError } from 'axios'
+import { isAxiosError } from 'axios'
 import { toast } from 'sonner'
+import api from '../config/axios'
 
 export default function RegisterView() {
   const initialValues: RegisterForm = {
@@ -26,10 +27,7 @@ export default function RegisterView() {
 
   const handleRegister = async (formData: RegisterForm) => {
     try {
-      const { data } = await axios.post(
-        `${import.meta.env.VITE_BASE_API_URL}/auth/register`,
-        formData
-      )
+      const { data } = await api.post('/auth/register', formData)
       toast.success(data.msg)
       reset()
     } catch (error) {
