4ffd38920dbeee5447c00b9e8b13ed64cb2a94a6
NahumFGz <ngutierrez2507@gmail.com>
2025-03-14 15:18:02 -0500

    feat: :sparkles: se agrega isAxiosError

--- a_me_devtree/frontend/src/views/RegisterView.tsx
+++ a_me_devtree/frontend/src/views/RegisterView.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import type { RegisterForm } from '../types'
 import { ErrorMessage } from '../components/ErrorMessage'
-import axios from 'axios'
+import axios, { isAxiosError } from 'axios'
 
 export default function RegisterView() {
   const initialValues: RegisterForm = {
@@ -24,10 +24,12 @@ export default function RegisterView() {
 
   const handleRegister = async (formData: RegisterForm) => {
     try {
-      const response = await axios.post('http://localhost:4000/auth/register', formData)
-      console.log(response)
+      const { data } = await axios.post('http://localhost:4000/auth/register', formData)
+      console.log(data)
     } catch (error) {
-      console.log(error)
+      if (isAxiosError(error) && error.response) {
+        console.log(error.response.data.error)
+      }
     }
   }
 
