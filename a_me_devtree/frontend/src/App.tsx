5004ad497da27032bf7f73ee0ef098d3e50c8188
NahumFGz <ngutierrez2507@gmail.com>
2025-03-13 21:56:00 -0500

    feat: :sparkles: Instalación de tailwind completada

--- a_me_devtree/frontend/src/App.tsx
+++ a_me_devtree/frontend/src/App.tsx
@@ -1,9 +1,11 @@
-import './App.css'
-
 function App() {
   const holaMundo = 'Hola Mundo desde TSX'
 
-  return <>{holaMundo}</>
+  return (
+    <>
+      <h1 className='text-3xl bg-red-500 font-bold underline'>{holaMundo}</h1>
+    </>
+  )
 }
 
 export default App
