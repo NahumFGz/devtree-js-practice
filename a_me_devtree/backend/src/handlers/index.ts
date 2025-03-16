e012602d181a55ec42481df2df105313bd9fbbb2
NahumFGz <ngutierrez2507@gmail.com>
2025-03-16 14:46:33 -0500

    feat: :sparkles: Se inicia la obtención de las apis de datos de usuario

--- a_me_devtree/backend/src/handlers/index.ts
+++ a_me_devtree/backend/src/handlers/index.ts
@@ -48,3 +48,12 @@ export const login = async (req: Request, res: Response) => {
   const token = generateJWT({ id: user._id })
   res.send(token)
 }
+
+export const getUser = async (req: Request, res: Response) => {
+  const bearer = req.headers.authorization
+
+  if (!bearer) {
+    const error = new Error('No autorizado')
+    return res.status(401).json({ error: error.message })
+  }
+}
