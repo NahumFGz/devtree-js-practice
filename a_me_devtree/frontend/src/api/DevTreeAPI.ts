47cf9e423f3e2ffbf71cdd0f58e178169f9c3f61
NahumFGz <ngutierrez2507@gmail.com>
2025-03-16 17:16:45 -0500

    feat: :sparkles: Se agregan los interceptor a axios

--- a_me_devtree/frontend/src/api/DevTreeAPI.ts
+++ a_me_devtree/frontend/src/api/DevTreeAPI.ts
@@ -1,19 +1,13 @@
-import { isAxiosError } from "axios"
-import api from "../config/axios"
+import { isAxiosError } from 'axios'
+import api from '../config/axios'
 
 export async function getUser() {
-    const token = localStorage.getItem('AUTH_TOKEN')
-    
-    try {
-        const { data } = await api('/user', {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-        return data
-      } catch (error) {
-        if (isAxiosError(error) && error.response) {
-          throw new Error(error.response.data.error)
-        }
-      }
+  try {
+    const { data } = await api('/user')
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
 }
