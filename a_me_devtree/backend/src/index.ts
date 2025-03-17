fcad387020d54d9787f064a225eca709cfb26f2c
NahumFGz <ngutierrez2507@gmail.com>
2025-03-17 15:23:50 -0500

    fix: :ambulance: Corrección de la carga de variables de entorno

--- a_me_devtree/backend/src/index.ts
+++ a_me_devtree/backend/src/index.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import colors from 'colors'
 import server from './server'
 
@@ -6,41 +9,3 @@ const port = process.env.PORT || 4000
 server.listen(port, () => {
   console.log(colors.blue.bold(`Servidor funcionando en el puerto: ${port}`))
 })
-
-let productName = 'Tablet'
-let isAuth = false
-let price = 30
-
-type Product = {
-  id: number
-  price: number
-  name: string
-}
-
-/*
-type ProductID = {
-  id: Product['id']
-}
-*/
-//type ProductID = Omit<Product, 'id'>
-//type ProductID = Omit<Product, 'id' | 'name'>
-//type ProductID = Pick<Product, 'id' | 'name'>
-type ProductID = Pick<Product, 'id'>
-
-let product3: ProductID = {
-  id: 1,
-}
-
-let product: Product = {
-  id: 1,
-  price: 30,
-  name: 'tablet',
-}
-
-let product2: Product = {
-  id: 2,
-  price: 30,
-  name: 'tablet 11 pulgadas',
-}
-
-const numbers = [10, 20, 30]
