551b674aa0505f06bf62e65100a9bf900ecd6fbe
NahumFGz <ngutierrez2507@gmail.com>
2025-03-18 16:20:37 -0500

    feat: :sparkles: Agregando el cambio de estado en los Switch

--- a_me_devtree/frontend/src/components/DevTreeInput.tsx
+++ a_me_devtree/frontend/src/components/DevTreeInput.tsx
@@ -5,9 +5,14 @@ import { DevTreeLink } from '../types'
 type DevTreeInputProps = {
   item: DevTreeLink
   handleUrlChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleEnableLink: (socialNetwork: string) => void
 }
 
-export default function DevTreeInput({ item, handleUrlChange }: DevTreeInputProps) {
+export default function DevTreeInput({
+  item,
+  handleUrlChange,
+  handleEnableLink
+}: DevTreeInputProps) {
   return (
     <div className='bg-white shadow-sm p-5 flex items-center gap-3'>
       <div
@@ -24,7 +29,7 @@ export default function DevTreeInput({ item, handleUrlChange }: DevTreeInputProp
 
       <Switch
         checked={item.enabled}
-        onChange={() => {}}
+        onChange={() => handleEnableLink(item.name)}
         className={classNames(
           item.enabled ? 'bg-blue-500' : 'bg-gray-200',
           'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
--- a_me_devtree/frontend/src/views/LinkTreeView.tsx
+++ a_me_devtree/frontend/src/views/LinkTreeView.tsx
@@ -5,8 +5,6 @@ import DevTreeInput from '../components/DevTreeInput'
 export default function LinkTreeView() {
   const [devTreeLinks, setDevTreeLinks] = useState(social)
 
-  console.log('devTreeLinks', devTreeLinks)
-
   const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const updatedLinks = devTreeLinks.map((link) =>
       link.name === e.target.name ? { ...link, url: e.target.value } : link
@@ -15,11 +13,25 @@ export default function LinkTreeView() {
     setDevTreeLinks(updatedLinks)
   }
 
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === socialNetwork ? { ...link, enabled: !link.enabled } : link
+    )
+
+    console.log(updatedLinks)
+    setDevTreeLinks(updatedLinks)
+  }
+
   return (
     <>
       <div className='space-y-5'>
         {devTreeLinks.map((item) => (
-          <DevTreeInput key={item.name} item={item} handleUrlChange={handleUrlChange} />
+          <DevTreeInput
+            key={item.name}
+            item={item}
+            handleUrlChange={handleUrlChange}
+            handleEnableLink={handleEnableLink}
+          />
         ))}
       </div>
     </>
