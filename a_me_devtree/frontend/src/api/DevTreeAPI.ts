2625eb435a46d6d2cbde6ef55c734030e3ad8631
NahumFGz <ngutierrez2507@gmail.com>
2025-03-19 17:27:42 -0500

    feat: :sparkles: Trayendo los datos del usuario

--- a_me_devtree/frontend/src/api/DevTreeAPI.ts
+++ a_me_devtree/frontend/src/api/DevTreeAPI.ts
@@ -39,3 +39,14 @@ export async function uploadImage(file: File) {
     }
   }
 }
+
+export async function getUserByHandle(handle: string) {
+  try {
+    const { data } = await api.get(`/${handle}`)
+    return data
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
--- a_me_devtree/frontend/src/views/HandleView.tsx
+++ a_me_devtree/frontend/src/views/HandleView.tsx
@@ -1,4 +1,19 @@
+import { useParams } from 'react-router-dom'
+import { useQuery } from '@tanstack/react-query'
+import { getUserByHandle } from '../api/DevTreeAPI'
+
 export default function HandleView() {
+  const params = useParams()
+  const handle = params.handle!
+  const { data, error, isLoading } = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ['handle', handle],
+    retry: 1
+  })
+
+  console.log('data: ', data)
+  console.log('error:', error)
+  console.log('isLoading: ', isLoading)
   return (
     <>
       <p>HandleView</p>
